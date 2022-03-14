e7957edbb4d88db3c9cc585a1b8baa94b5b9ba4d
Samba Bery KANE <sambabery.kane@gmail.com>
2022-03-14 12:37:10 +0000

    Formulaire enregistrement entreprises

--- app/Http/Controllers/EntrepriseController.php
+++ app/Http/Controllers/EntrepriseController.php
@@ -2,15 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Entreprise;
+use App\Models\Quartier;
 use Illuminate\Http\Request;
+use Symfony\Component\Console\Input\Input;
 
 class EntrepriseController extends Controller
 {
+
     public function index () {
-        return view('entreprises.index');
+        return view('entreprises.index', [
+            'entreprises' => Entreprise::all()
+        ]);
     }
 
     public function create() {
-        return view('entreprises.create');
+        $quartiers = Quartier::all();
+        return view('entreprises.create',[
+            'quartiers' => $quartiers
+        ]);
+    }
+
+    public function store(Request $request) {
+
+        //TODO: Refactoring cette partie de code
+        $inputsData = $request->all();
+        $inputsData['dispositifFormation'] = $request->has('dispositifFormation') ? true : false;
+        $inputsData['organigramme'] = $request->has('organigramme') ? true : false;
+        $inputsData['contrat'] = $request->has('contrat') ? true : false;
+
+        Entreprise::create($inputsData);
+        return redirect()->route('entreprises.index');
     }
 }
